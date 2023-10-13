6d45ba2c135d20a905edb17af1caf305304bb5b8
Bruna Finardi <145575249+finardib@users.noreply.github.com>
2023-10-13 13:59:57 -0300

    Update Estacionamento.cs

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -29,26 +29,29 @@ namespace DesafioFundamentos.Models
         }
 
         public void RemoverVeiculo()
-        {
-                Console.WriteLine("Digite a placa do veículo para remover:");
-                string placa = Console.ReadLine();
-                    
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
-            {
-                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
-                int horas = Convert.ToInt32(Console.ReadLine());
+{
+    Console.WriteLine("Digite a placa do veículo para remover:");
+    string placa = Console.ReadLine();
 
-                decimal valorTotal = precoInicial + precoPorHora * horas;
+    int indiceVeiculo = veiculos.FindIndex(x => x.ToUpper() == placa.ToUpper());
 
-                veiculos.Remove(placa.ToUpper()); 
+    if (indiceVeiculo >= 0)
+    {
+        Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+        int horas = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
-            }
-            else
-            {
-                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
-            }
-        }
+        decimal valorTotal = precoInicial + precoPorHora * horas;
+
+        veiculos.RemoveAt(indiceVeiculo); // Remove o veículo da lista pelo índice
+
+        Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+    }
+    else
+    {
+        Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+    }
+}
+
 
         public void ListarVeiculos()
         {
